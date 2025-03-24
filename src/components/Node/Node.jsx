0b1e7ba73243f807a07c182db145c150abe2ffeb
Satya Sai Koppalu <satya.reddy62@gmail.com>
2025-03-23 18:28:42 -0700

    Updated code

--- src/components/Node/Node.jsx
+++ src/components/Node/Node.jsx
@@ -13,15 +13,15 @@ export default class Node extends Component{
             onMouseUp,
             onMouseEnter
         }=this.props;
-        const extraClassName=isFinish?"node-finish":isStart?"node-start":isWall?"isWall":"";
+        const extraClassName=isFinish?"node-finish":isStart?"node-start":isWall?"node-wall":"";
 
         return (
-            <div>
+            <div
                 id={`node-${row}-${col}`}
                 className={`node ${extraClassName}`}
                 onMouseDown={()=>onMouseDown(row, col)}
                 onMouseEnter={()=>onMouseEnter(row, col)}
-                onMouseUp={()=>onMouseUp()}
+                onMouseUp={()=>onMouseUp()}>
             </div>
 
         );
